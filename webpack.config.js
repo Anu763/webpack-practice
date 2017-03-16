650917f69c418559aea744c8188ccde0449ae547
Anu <anu@it-consultis.com>
2017-03-16 12:09:24 +0800

    added index.html node_modules package.json and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var webpackConfig = {
+    entry: "./js/index.js",
+    output: {
+        path: "build",
+        filename: "build.js"
+    },
+    module: {
+        loaders: [
+            {
+                loader: ""
+            }
+        ]
+    }
+}
